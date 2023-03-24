f846137a3c72c8a73a9a57a9a57af431b9434ee2
Fernando Mirabile <fernando.mirabile@split.io>
2023-03-24 18:33:32 -0300

    Some new pages added with libraries

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,17 +10,9 @@
 	<main>
 		<slot />
 	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
-<style lang="postcss">
-	/* :global(html) {
-		background-color: theme(colors.gray.100);
-	} */
-
+<style>
 	.app {
 		display: flex;
 		flex-direction: column;
@@ -36,18 +28,6 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
 	}
 
 	@media (min-width: 480px) {
