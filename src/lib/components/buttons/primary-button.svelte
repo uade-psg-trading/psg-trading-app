96f35f71253ab1a7d7e4da8c93e649cb1ad4c0dc
Fernando Mirabile <fpmirabile@gmail.com>
2023-03-31 17:36:06 -0300

    Fixing linting issues

--- src/lib/components/buttons/primary-button.svelte
+++ src/lib/components/buttons/primary-button.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import type { HTMLButtonAttributes } from 'svelte/elements';
 
-  export let onClick = () => {};
   export let title: string;
   export let buttonType: HTMLButtonAttributes['type'] = 'button';
 </script>
@@ -9,7 +8,7 @@
 <button
   type={buttonType}
   class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-  on:click={onClick}
+  on:click
 >
   <slot name="icon" />
   {title}
--- src/lib/components/buttons/security-button.svelte
+++ src/lib/components/buttons/security-button.svelte
@@ -3,12 +3,11 @@
   import LockIcon from '$lib/icons/lock.svelte';
   import type { HTMLButtonAttributes } from 'svelte/elements';
 
-  export let onClick = () => {};
   export let title: string;
   export let buttonType: HTMLButtonAttributes['type'] = 'button';
 </script>
 
-<PrimaryButton {onClick} {title} {buttonType}>
+<PrimaryButton on:click {title} {buttonType}>
   <span slot="icon" class="absolute inset-y-0 left-0 flex items-center pl-3">
     <LockIcon />
   </span>
