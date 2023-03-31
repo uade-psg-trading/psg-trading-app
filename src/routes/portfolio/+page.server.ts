6a52089605a39f4152d0e82020f26f1c6aecaa2c
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-03-31 08:40:17 -0300

    Add width fix

--- src/routes/portfolio/+page.server.ts
+++ src/routes/portfolio/+page.server.ts
@@ -1,7 +1,6 @@
-import { redirect, fail } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
-/** @type {import('./$types').Actions} */
 export const actions: Actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
