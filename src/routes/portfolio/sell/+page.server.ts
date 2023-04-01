aef04341e01e8bc20a87ae0ca4910b878f0f1b2a
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-01 11:29:31 -0300

    Fix format

--- src/routes/portfolio/sell/+page.server.ts
+++ src/routes/portfolio/sell/+page.server.ts
@@ -2,21 +2,21 @@ import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
-	default: async ({ request }) => {
-		const formData = await request.formData();
-		const token = formData.get('token') as string;
-		const amount = formData.get('amount') as string;
-		const errors: Record<string, unknown> = {};
-		if (!token || !amount) {
-			errors.name = 'required';
-		}
+  default: async ({ request }) => {
+    const formData = await request.formData();
+    const token = formData.get('token') as string;
+    const amount = formData.get('amount') as string;
+    const errors: Record<string, unknown> = {};
+    if (!token || !amount) {
+      errors.name = 'required';
+    }
 
-		if (Object.keys(errors).length > 0) {
-			return fail(400, errors);
-		}
+    if (Object.keys(errors).length > 0) {
+      return fail(400, errors);
+    }
 
-		return {
-			message: 'Not implemented'
-		};
-	}
+    return {
+      message: 'Not implemented'
+    };
+  }
 };
