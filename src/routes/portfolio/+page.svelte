7d53587c16b02cec452f9a731fba97629d50a7bc
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-01 11:29:31 -0300

    Fix format

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,11 +1,11 @@
 <svelte:head>
-	<title>Vender Activo</title>
+  <title>Vender Activo</title>
 </svelte:head>
 
 <div class="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-	<div class="rounded p-6 bg-white w-full max-w-screen-lg space-y-8">
-		<div class="buttons">
-			<a href="/portfolio/sell"><button class="large row">Vender</button></a>
-		</div>
-	</div>
+  <div class="rounded p-6 bg-white w-full max-w-screen-lg space-y-8">
+    <div class="buttons">
+      <a href="/portfolio/sell"><button class="large row">Vender</button></a>
+    </div>
+  </div>
 </div>
