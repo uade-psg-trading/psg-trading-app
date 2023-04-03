6bd216a3071290135849069a23fa5768c186f45e
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-02 22:59:07 -0300

    Remove unused line

--- src/routes/portfolio/sell/+page.server.ts
+++ src/routes/portfolio/sell/+page.server.ts
@@ -5,11 +5,10 @@ import type { Actions } from './$types';
 export const actions: Actions = {
   default: async ({ request }) => {
     const formData = await request.formData();
-    const token = formData.get('token') as string;
     const amount = formData.get('amount') as string;
-    const selectedValue = formData.get('tokenSelection') as string;
+    const selectedToken = formData.get('tokenSelection') as string;
     const errors: Record<string, unknown> = {};
-    if (!token || !amount) {
+    if (!selectedToken || !amount) {
       errors.name = 'required';
     }
 
@@ -18,7 +17,7 @@ export const actions: Actions = {
     }
 
     return {
-      message: selectedValue
+      message: selectedToken
     };
   }
 };
