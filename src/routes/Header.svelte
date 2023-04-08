d449912bf7a934c07db4a7dc6e12347de9e1956f
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-08 14:03:38 -0300

    Fix header lint

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -87,7 +87,7 @@
                 href="/cash-in"
                 class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                 >Ingresar Dinero</a
-              >              
+              >
             {/if}
             <a
               href="#"
