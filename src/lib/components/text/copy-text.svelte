37e0af7271e48aa766298b3beec5058b39b80823
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-09 11:01:48 -0300

    Add copy button

--- src/lib/components/text/copy-text.svelte
+++ src/lib/components/text/copy-text.svelte
@@ -3,6 +3,9 @@
   import copyButton from '$lib/images/copy-button.svg';
   export let title: string;
   export let text: string;
+  function handleCopy() {
+    navigator.clipboard.writeText(text);
+  }
 </script>
 
 <div class="flex items-center mt-4">
@@ -10,5 +13,7 @@
     <span class="text-base text-gray-900">{title}:</span>
     <span class="text-sm text-gray-500 mr-1.5">{text}</span>
   </p>
-  <img class="w-4 h-4 " src={copyButton} alt="Copy" />
+  <button on:click={handleCopy}>
+    <img class="w-4 h-4 " src={copyButton} alt="Copy" />
+  </button>
 </div>
