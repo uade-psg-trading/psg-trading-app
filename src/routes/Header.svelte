c299f14521e8a598bef850f9179a0da0e95cd605
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-13 20:34:06 -0300

    Add notification page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -98,8 +98,8 @@
         <div
           class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
-          <button
-            type="button"
+          <a
+            href="/notifications"
             class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
           >
             <span class="sr-only">View notifications</span>
@@ -117,7 +117,7 @@
                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
               />
             </svg>
-          </button>
+          </a>
 
           <!-- Profile dropdown -->
           <div on:click={toggleMenu} class="relative ml-3">
---
+++ src/routes/notifications/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import WhiteCard from '$lib/components/cards/white-card.svelte';
+  import { headerStore } from '$lib/stores';
+  import { enhance } from '$app/forms';
+  import FormInput from '$lib/components/input/input-with-title.svelte';
+  import PrimaryButton from '$lib/components/buttons/primary-button.svelte';
+  import Table from '$lib/components/table/table.svelte';
+  import Selector from '$lib/components/selector/selector.svelte';
+
+  onMount(() => {
+    headerStore.update((value) => {
+      value.title = 'Mis Alarmas';
+      return value;
+    });
+  });
+  let selectedValue: string;
+  let tokenList = [`$PSG`, `$BAR`, `$CITY`];
+  const columns = [
+    { key: 'id', title: 'ID', value: (row) => row.id, visible: false },
+    { key: 'tokenName', title: 'Simbolo', value: (row) => row.tokenName },
+    { key: 'reason', title: 'Avisarme si', value: (row) => row.reason },
+    { key: 'amount', title: 'Valor', value: (row) => row.amount }
+  ];
+  const rows = [
+    {
+      id: 1,
+      tokenName: '$PSG',
+      reason: 'BAJA',
+      amount: '100'
+    },
+    {
+      id: 2,
+      tokenName: '$BAR',
+      reason: 'BAJA',
+      amount: '150'
+    }
+  ];
+</script>
+
+<svelte:head>
+  <title>Mis Alarmas</title>
+</svelte:head>
+
+<div class="container">
+  <div class="py-12">
+    <WhiteCard>
+      <div class="mb-4">
+        <h2 class="text-left text-lg font-bold text-indigo-800">Agregar una alarma</h2>
+        <form action="/notification" method="POST" class="w-full" use:enhance>
+          <div class="flex -mx-3 mt-4">
+            <div class="w-1/4 mb-4 px-3">
+              <Selector
+                id="tokenName"
+                name="tokenName"
+                value={selectedValue}
+                list={tokenList}
+                labelTitle="Simbolo"
+              />
+            </div>
+            <div class="w-1/4 mb-4 px-3">
+              <Selector
+                id="reason"
+                name="reason"
+                value={selectedValue}
+                list={tokenList}
+                labelTitle="Avisarme si"
+              />
+            </div>
+            <div class="w-1/4 mb-4 px-3">
+              <FormInput
+                id="amount"
+                name="amount"
+                isRequired={true}
+                labelTitle="Valor"
+                type="number"
+              />
+            </div>
+            <div class="w-1/4 px-3 mt-8">
+              <PrimaryButton title="Agregar" buttonType="submit" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </WhiteCard>
+  </div>
+
+  <WhiteCard>
+    <h2 class="text-left text-lg font-bold text-indigo-800">Mis alarmas configuradas</h2>
+    <Table showOptionsMenu={true} {rows} {columns} />
+  </WhiteCard>
+</div>
