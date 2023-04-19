201be195cd3f085ddee146ec48eff477b9c4da1c
Fernando Mirabile <fpmirabile@gmail.com>
2023-04-19 20:42:04 -0300

    Fixing e2e

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,3 +1,3 @@
-<div class="flex justify-center items-center min-h-screen bg-gray-50">
+<div data-testid="login-layout" class="flex justify-center items-center min-h-screen bg-gray-50">
   <slot />
 </div>
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('about page has expected h1', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByTestId('test')).toBeVisible();
+  await page.goto('/sign-in');
+  await expect(page.getByTestId('login-layout')).toBeVisible();
 });
