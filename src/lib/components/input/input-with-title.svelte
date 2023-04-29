5fcfe254b488704ce9fb390eb8f21930b538a15a
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-29 11:57:06 -0300

    Improve names

--- src/lib/components/input/input-with-title.svelte
+++ src/lib/components/input/input-with-title.svelte
@@ -9,17 +9,17 @@
   export let type: HTMLInputAttributes['type'] = 'text';
   export let value = '';
   export let readonly = false;
-  const classValue =
+  const classBase =
     'appearance-none block w-full  text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500';
-  const classValueDisabled = `${classValue} bg-gray-300`;
-  const classValueEnabled = `${classValue} bg-white`;
+  const classReadOnly = `${classBase} bg-gray-300`;
+  const classDefault = `${classBase} bg-white`;
 </script>
 
 <label class="block tracking-wide text-gray-700 text-sm mb-2" for={name}>
   {labelTitle}
 </label>
 <input
-  class={readonly ? classValueDisabled : classValueEnabled}
+  class={readonly ? classReadOnly : classDefault}
   {id}
   {name}
   {type}
