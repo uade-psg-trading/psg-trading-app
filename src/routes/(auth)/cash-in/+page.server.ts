032f6ff7ab75772008cbc528172bf073ba7ba7ed
Gonzalo Iglesias <gonzalo.iglesias@pomelo.la>
2023-04-30 11:28:30 -0300

    Fix integration

--- src/routes/(auth)/cash-in/+page.server.ts
+++ src/routes/(auth)/cash-in/+page.server.ts
@@ -1,23 +1,43 @@
-import { fail } from '@sveltejs/kit';
+import { apiEndpoints } from '$lib/api';
+import { getCurrentSession } from '$lib/server/cookie-manager';
+import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
-/** @type {import('./$types').Actions} */
+type CashInForm = {
+  amount: string;
+  tokenSelection: string;
+};
+
 export const actions: Actions = {
-  default: async ({ request }) => {
+  default: async ({ cookies, locals, request }) => {
     const formData = await request.formData();
-    const amount = formData.get('amount') as string;
-    const fullName = formData.get('full_name') as string;
+    const cashInForm = Object.fromEntries(formData) as CashInForm;
     const errors: Record<string, unknown> = {};
-    if (!amount || !fullName) {
-      errors.name = 'required';
-    }
 
     if (Object.keys(errors).length > 0) {
       return fail(400, errors);
     }
 
-    return {
-      message: fullName
-    };
+    const jwt = getCurrentSession(cookies, locals);
+    if (jwt == null) {
+      return fail(400, {
+        errors: {
+          message: 'Error with token'
+        }
+      });
+    }
+    const createPaymentResponse = await apiEndpoints.payments.createPayment(jwt, {
+      amount: Number(cashInForm.amount),
+      paymentMethod: 'CREDIT_CARD'
+    });
+    if (createPaymentResponse.success) {
+      throw redirect(303, '/');
+    }
+
+    return fail(400, {
+      errors: {
+        message: createPaymentResponse.message
+      }
+    });
   }
 };
