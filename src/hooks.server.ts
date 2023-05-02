ee3f3bf47e5a13d7d8ca79c6944116b9e92b083a
Fernando Mirabile <fpmirabile@gmail.com>
2023-05-02 17:53:42 -0300

    Cleaning console.log

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,19 +6,14 @@ import { redirect, type Handle } from '@sveltejs/kit';
 export const handle = (async ({ event, resolve }) => {
   const { cookies, locals, url } = event;
   const jwt = getCurrentSession(cookies, locals);
-  console.log('hook jwt', jwt, cookies.getAll());
   if (jwt) {
     const currentUserData = await apiEndpoints.user.me(jwt);
-    console.log('me', currentUserData);
     const tenant = currentUserData.success
       ? ((currentUserData.data?.tenantId === 'default'
           ? 'trading'
           : currentUserData.data?.tenantId ?? 'trading') as TenantType)
       : getTenant(url, locals);
-    console.log('tenant', tenant);
     setCurrentTenant(locals, tenant);
-
-    console.log('isTenantUrl', isTenantUrl(url, tenant));
     if (!isTenantUrl(url, tenant)) {
       const tenantUrl = `${url.protocol}//${tenant}.${url.hostname}:${url.port}${url.pathname}`;
       console.log('tenantUrl', tenantUrl);
